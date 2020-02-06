b065c784fe99f4899cf26760d757bc08fcf03161
ReturnZer0 <raofei23@gmail.com>
2020-02-06 12:00:08 +0800

    modify header

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,51 @@
   <el-container>
     <el-header>
       <el-row>
-        <el-col :span="24">
-          <h2>何为净土</h2>
+        <el-col :span="3">
+          <div>
+            <h2>PureLand</h2>
+          </div>
+        </el-col>
+        <el-col :span="5">
+          <div>
+            <el-input v-model="input" placeholder="请输入搜索内容"></el-input>
+          </div>
+        </el-col>
+        <el-col :span="16">
+          <div>
+            <ul>
+              <li>
+                <span>
+                  <i class="el-icon-set-up"></i>
+                  <span>全选</span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <i class="el-icon-office-building"></i>
+                  <span>小区</span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <i class="el-icon-school"></i>
+                  <span>医院</span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <i class="el-icon-guide"></i>
+                  <span>公交</span>
+                </span>
+              </li>
+              <li>
+                <span>
+                  <i class="el-icon-shopping-cart-2"></i>
+                  <span>商场</span>
+                </span>
+              </li>
+            </ul>
+          </div>
         </el-col>
       </el-row>
     </el-header>
@@ -46,5 +89,18 @@ export default class App extends Vue {}
 <style>
 h2 {
   text-align: center;
+  margin: 4px; /* 将默认的外边距去掉 */
+}
+
+ul {
+  margin: 6px; /* 将默认的外边距去掉 */
+}
+
+li {
+  list-style: none; /* 将默认的列表符号去掉 */
+  padding: 0; /* 将默认的内边距去掉 */
+  margin: 0; /* 将默认的外边距去掉 */
+  float: left; /* 往左浮动 */
+  width: 70px;
 }
 </style>
