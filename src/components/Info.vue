80ae505ca687cf42430264df5fcc501a28b7abc3
ReturnZer0 <raofei23@gmail.com>
2020-02-06 22:05:29 +0800

    show trainList

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -11,19 +11,56 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import { DataStore } from '../services/data-store';
 
+interface Params {
+  componentType: string;
+  componentSubType: string;
+  componentIndex: number;
+  seriesType: string;
+  seriesIndex: number;
+  seriesId: string;
+  seriesName: string;
+  name: string;
+  dataIndex: number;
+  type: string;
+}
+
 @Component
 export default class Info extends Vue {
   info: any = {};
-  ms = ['第一个', '第二个'];
+  ms: string[] = [];
+
+  trainInfoLists: {
+    name: string;
+    list: {
+      train: string;
+      carriage: string;
+      time: string;
+    }[];
+  }[] = require('../assets/data/railwaystations.json');
 
   constructor() {
     super();
   }
+
   mounted() {
     DataStore.infoEmitter.asObservable().subscribe(info => {
       console.log(info, '检测 info 变化');
-      this.info = info;
+      // this.info = info;
+      const trainStationName = (info as Params).name;
+      this.info.name = trainStationName + ' 患者同行列车信息';
+      this.ms = this.getTrainList(trainStationName);
     });
+  }
+
+  getTrainList(trainStationName: string) {
+    console.log('trainStationName:' + trainStationName);
+    for (let trainList of this.trainInfoLists) {
+      if (trainList.name === trainStationName)
+        return trainList.list.map(
+          trainInfo => '车次:' + trainInfo.train + ' 时间:' + trainInfo.time
+        ) as string[];
+    }
+    return [];
   }
 }
 </script>
